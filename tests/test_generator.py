eb5a21bf982bd148bb3a32ab06ed35691314886f
SergeKapinus <kapinus.serg@gmail.com>
2023-10-24 10:36:10 +0300

    fix test_generator.py

--- tests/test_generator.py
+++ tests/test_generator.py
@@ -2,7 +2,7 @@ from gendiff.generator import generate_diff
 
 
 def test_generate_diff():
-    assert generate_diff("fixtures/test1.json", "fixtures/test2.json") == """{
+    assert generate_diff("tests/fixtures/test1.json", "tests/fixtures/test2.json") == """{
   - follow: false
     host: hexlet.io
   - proxy: 123.234.53.22
@@ -10,9 +10,9 @@ def test_generate_diff():
   + timeout: 20
   + verbose: true
 }"""
-    assert generate_diff("fixtures/test1.json", "fixtures/test3.json") == """{
-    follow: false       
-    host: hexlet.io     
+    assert generate_diff("tests/fixtures/test1.json", "tests/fixtures/test3.json") == """{
+    follow: false
+    host: hexlet.io
     proxy: 123.234.53.22
     timeout: 50
 }"""
