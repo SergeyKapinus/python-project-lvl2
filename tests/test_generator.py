a58f774021854b647dd2e2c496f331d20dc8b97a
Капинус Сергей Валерьевич <SeVKapinus@atex.local>
2023-11-01 13:56:09 +0300

    step 5

--- tests/test_generator.py
+++ tests/test_generator.py
@@ -16,6 +16,12 @@ result2 = """{
     timeout: 50
 }"""
 
-def test_generate_diff():
+
+def test_generate_diff_json():
     assert generate_diff("tests/fixtures/test1.json", "tests/fixtures/test2.json") == result1
     assert generate_diff("tests/fixtures/test1.json", "tests/fixtures/test3.json") == result2
+
+
+def test_generate_diff_yaml():
+    assert generate_diff("tests/fixtures/test1.yml", "tests/fixtures/test2.yml") == result1
+    assert generate_diff("tests/fixtures/test1.yml", "tests/fixtures/test3.yml") == result2
